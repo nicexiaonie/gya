d68c43690cf134e6f2cf98176af33dafa4019633
nieyuanpei@gmail.com <nieyuanpei@gmail.com>
2025-03-17 15:23:34 +0800

    aa

--- gya.go
+++ gya.go
@@ -1,12 +1,26 @@
 package gya
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func Init() {
+
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Println("获取可执行文件路径失败:", err)
+		return
+	}
+
+	// 获取当前可执行文件所在的目录
+	dir := filepath.Dir(exePath)
+	fmt.Println("当前文件所在目录:", dir)
 
 }
 
 func Run() {
-    basePath, _ := os.Getwd()
-    println(basePath)
+	basePath, _ := os.Getwd()
+	println(basePath)
 }
